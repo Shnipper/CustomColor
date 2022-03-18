a8a7e384d9002cca0cc0b8af3751fe0bde133849
Voloshenko Evgeniu <95745884+Shnipper@users.noreply.github.com>
2022-03-18 14:09:35 +0300

    Functionality added

--- CustomColor/ViewController.swift
+++ CustomColor/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redPoints: UILabel!
+    @IBOutlet weak var greenPoints: UILabel!
+    @IBOutlet weak var bluePoints: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redPoints.text = String(round(redSlider.value * 100) / 100)
+        greenPoints.text = String(round(greenSlider.value * 100) / 100)
+        bluePoints.text = String(round(blueSlider.value * 100) / 100)
     }
-
-
+    
+    private func colorChanged() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
+    
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        redPoints.text = String(round(redSlider.value * 100) / 100)
+        colorChanged()
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenPoints.text = String(round(greenSlider.value * 100) / 100)
+        colorChanged()
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        bluePoints.text = String(round(blueSlider.value * 100) / 100)
+        colorChanged()
+    }
 }
+
 
