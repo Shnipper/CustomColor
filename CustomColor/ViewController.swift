14a6ec8cc1f60cfdf47e31a8c6efe052a43c9135
Voloshenko Evgeniu <95745884+Shnipper@users.noreply.github.com>
2022-03-18 15:15:03 +0300

    bugfix

--- CustomColor/ViewController.swift
+++ CustomColor/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
+
     @IBOutlet weak var redPoints: UILabel!
     @IBOutlet weak var greenPoints: UILabel!
     @IBOutlet weak var bluePoints: UILabel!
+
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
@@ -22,13 +24,6 @@ class ViewController: UIViewController {
         redPoints.text = String(round(redSlider.value * 100) / 100)
         greenPoints.text = String(round(greenSlider.value * 100) / 100)
         bluePoints.text = String(round(blueSlider.value * 100) / 100)
-    }
-    
-    private func colorChanged() {
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                            green: CGFloat(greenSlider.value),
-                                            blue: CGFloat(blueSlider.value),
-                                            alpha: 1)
     }
     
     @IBAction func redSliderChanged(_ sender: UISlider) {
@@ -45,6 +40,11 @@ class ViewController: UIViewController {
         bluePoints.text = String(round(blueSlider.value * 100) / 100)
         colorChanged()
     }
+
+    private func colorChanged() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
 }
-
-
